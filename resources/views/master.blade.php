265c111fec93d66b97aa044908c0554a2b74d52f
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-04 16:22:54 -0600

    New view (views): :sparkles: master view created

---
+++ resources/views/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Master</title>
+</head>
+<body>
+    <header>
+        Headrer
+    </header>
+
+    @yield('contenct')
+
+    <section>
+        @yield('morecontenct')
+    </section>
+</body>
+</html>
