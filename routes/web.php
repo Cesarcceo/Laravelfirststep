f61235671e7fc8e82e2c622aaefd2d0d11aedafe
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-04 14:17:05 -0600

    New route (web.php): :sparkles: new route for the crud

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/test', function (){
     return View('test');
 });
 
+Route::get('/crud', function (){
+    return View('crud/index');
+});
+
 // Route::get('/test', function(){
 //     return "Welcome";
 // });
