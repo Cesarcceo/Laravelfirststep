d36736bab98316b1f6874b9c7d756a1c91b3459b
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-04 14:22:10 -0600

    New feature (web.php): :sparkles: the view recive the data

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function (){
-    return View('test');
-});
+// Route::get('/test', function (){
+//     return View('test');
+// });
 
-Route::get('/crud', function (){
-    return View('crud/index');
-});
+// Route::get('/crud', function (){
+//     return View('crud/index');
+// });
 
 // Route::get('/test', function(){
 //     return "Welcome";
 // });
+
+Route::get('/crud', function(){
+    $age = 33;
+    $data = ['name' => 'Andres', 'age' => $age];
+
+    return view('crud/index', $data);
+});
