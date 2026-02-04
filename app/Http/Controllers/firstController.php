f41d3532358aca54461c708c44d45db94ea48c63
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-04 17:06:22 -0600

    Refactor code (firstController.php): :hamer: implemented compact

--- app/Http/Controllers/firstController.php
+++ app/Http/Controllers/firstController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 class firstController extends Controller{
 
     function index() {
-        return view('test');
+        $post = ['post1', 'post2'];
+        // return view('test', ['post'=>$post]);
+        return view('test', compact('post','categories'));
     }
     function other($post) {
         echo $post;
