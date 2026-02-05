42442f32b713db6d6ed10ed7c35f856cd0047fce
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-05 10:50:08 -0600

    New View (dashboard/post): :sparkles: create view created

---
+++ resources/views/dashboard/post/create.blade.php
@@ -0,0 +1,31 @@
+@extends('../../master')
+
+@section('contenct')
+    <form action="" method="post">
+
+        <label for="">Title</label>
+        <input type="text" name="title">
+
+        <label for="">Slug</label>
+        <input type="text" name="slug">
+
+        <label for="">Content</label>
+        <textarea name="content" id=""></textarea>
+        
+        <label for="">Categorie</label>
+        <select name="categorie_id" id="">
+            
+        </select>
+        
+        <label for="">Description</label>
+        <textarea name="description" id=""></textarea>
+
+        <label for="">Posted</label>
+        <select name="posted" id="">
+            <option value="not">Not</option>
+            <option value="yes">Yes</option>
+        </select>
+        
+        <button type=submit >send</button>
+    </form>
+@endsection
