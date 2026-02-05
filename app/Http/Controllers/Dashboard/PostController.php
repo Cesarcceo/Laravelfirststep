e87018a5595aded0bedfa17f0c85f8e53066c458
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-05 16:29:38 -0600

    New feature (PostControlles.php): :sparkles: now can upload a image

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -137,7 +137,14 @@ class PostController extends Controller
      */
     public function update(PutRequest $request, Post $post)
     {
-        $post->update($request->validated());
+        $data = $request->validated();
+
+        if (isset($data['image'])){
+            $data['image'] = $filename = time().'.'.$data['image']->extension();
+            $request->image->move(public_path('uploads/posts'),$filename);
+        }
+
+        $post->update($data);
         return to_route('post.index');
     }
 
