b8d056dbf770f31d6731f6d54955372246355ac9
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-05 15:22:22 -0600

    Refactor code (create.blade.php, edit.balde.php): :hammer: inclue the _form fragment

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -6,33 +6,6 @@
 
     <form action="{{ route('post.store') }}" method="post">
 
-        @csrf
-
-        <label for="">Title</label>
-        <input type="text" name="title">
-
-        <label for="">Slug</label>
-        <input type="text" name="slug">
-
-        <label for="">Content</label>
-        <textarea name="content" id=""></textarea>
-        
-        <label for="">Categorie</label>
-        <select name="categorie_id" id="">
-            @foreach($categories as $title => $id)
-                <option value="{{ $id }}">{{ $title }}</option>
-            @endforeach
-        </select>
-        
-        <label for="">Description</label>
-        <textarea name="description" id=""></textarea>
-
-        <label for="">Posted</label>
-        <select name="posted" id="">
-            <option value="not">Not</option>
-            <option value="yes">Yes</option>
-        </select>
-        
-        <button type=submit >send</button>
+        @include('dashboard.fragment._form')
     </form>
 @endsection
