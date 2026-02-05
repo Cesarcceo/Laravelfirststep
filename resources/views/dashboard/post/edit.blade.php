26b28fdd0845b1d79a3fb33589a7d47ec617520c
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-05 16:26:39 -0600

    New feature (edit.balde.php): :sparkles: a new variable is sent to the _form

--- resources/views/dashboard/post/edit.blade.php
+++ resources/views/dashboard/post/edit.blade.php
@@ -4,10 +4,10 @@
 
     @include('dashboard.fragment._errors-form')
 
-    <form action="{{ route('post.update', $post->id) }}" method="post">
+    <form action="{{ route('post.update', $post->id) }}" method="post" enctype="multipart/form-data">
 
         @method('PUT')
 
-        @include('dashboard.fragment._form')
+        @include('dashboard.fragment._form', [ 'task'=>'edit' ])
     </form>
 @endsection
