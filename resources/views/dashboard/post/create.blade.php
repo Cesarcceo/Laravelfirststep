5757914d88024d6829d89a9e3f928964e7b74317
Cesarcceo <cesar.consuelo@cceo.com.mx>
2026-02-05 12:58:09 -0600

    Refactor code (create.blade.php): :sparkles: change the foreach for an include

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -2,11 +2,7 @@
 
 @section('contenct')
 
-    @if($errors->any())
-        @foreach ($errors->all() as $e)
-            <div class="error">{{ $e }}</div>
-        @endforeach
-    @endif
+    @include('dashboard.fragment._errors-form')
 
     <form action="{{ route('post.store') }}" method="post">
 
